597e23ca37e1c8d2d42a80c53586216deb59530c
itggot-Noel-Ekbrand <noel.ekbrand@itggot.se>
2018-04-12 10:38:50 +0200

    header

---
+++ app.rb
@@ -0,0 +1,72 @@
+require 'sinatra'
+class App < Sinatra::Base
+enable :sessions
+require 'open-uri'
+require 'json'
+require 'addressable'
+require 'uri'
+
+	get ('/') do
+		slim(:index)
+	end
+
+	get('/register') do
+		slim(:register)
+	end
+
+	post('/register') do	
+		username = params["username"]
+		password = params["password"]
+		cryptpassword = BCrypt::Password.create(password)
+		dbcooper = SQLite3::Database.new("forum.sqlite")
+		namelist = dbcooper.execute("SELECT username FROM users")
+		namelist.each do |x|
+			if username == x.join
+				redirect('/fail')
+			end
+		end
+		dbcooper.execute("INSERT INTO users('username','password') VALUES(?,?)", [username,cryptpassword.to_s])
+		redirect('/')
+	end
+
+
+	post('/login')	do
+		username = params["username"]
+		password = params["password"]
+		dbcooper = SQLite3::Database.new("forum.sqlite")
+		password_digest = dbcooper.execute("SELECT password FROM users WHERE username = '#{username}'")
+		password_digest = BCrypt::Password.new(password_digest.join)
+		if password_digest == password
+			session[:user] = username
+			redirect('/logged_in/')
+		else
+		redirect('/fail')
+		end
+	end
+
+	get('/logged_in/') do
+		dbcooper = SQLite3::Database.new("forum.sqlite")
+		username = session[:user]
+		p username
+		userid = dbcooper.execute("SELECT id FROM users WHERE username 	= '#{username}'").join
+		info = dbcooper.execute("SELECT info FROM todo WHERE userid = '#{userid}'")
+		info_id = dbcooper.execute("SELECT id FROM todo WHERE userid = '#{userid}'")
+		slim(:to, locals:{ info:info, info_id:info_id})
+	end
+
+	post('/create') do
+		username = session[:user]
+		newinfo = params["newinfo"]
+		dbcooper = SQLite3::Database.new("forum.sqlite")
+		userid = dbcooper.execute("SELECT id FROM users WHERE username = '#{username}'").join
+		dbcooper.execute("INSERT INTO todo ('info', 'userid') VALUES (?,?)", [newinfo, userid])
+		redirect('/logged_in/')
+	end
+
+	post('/logout') do
+		session[:user] = nil
+		redirect('/')
+	end	
+
+
+end           
---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+<head>
+<script src="jquery-3.3.1.min.js"> </script>
+</head>
+
+$(".Login").click(function(){
+    $("button").hide();
+});
+
+$(".Login").click(function(){
+    $("button").show();
+});
