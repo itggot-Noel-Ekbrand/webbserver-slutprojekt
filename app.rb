ebe1c4ca31efd6609d01ecf1619efe961d3413cd
itggot-Noel-Ekbrand <noel.ekbrand@itggot.se>
2018-04-23 14:14:15 +0200

    tjavgd

--- app.rb
+++ app.rb
@@ -48,10 +48,11 @@ require 'uri'
 		dbcooper = SQLite3::Database.new("db/forum.sqlite")
 		username = session[:user]
 		p username
-		userid = dbcooper.execute("SELECT id FROM users WHERE username 	= '#{username}'").join
+		userid = dbcooper.execute("SELECT id FROM users WHERE username 	= ?", username).join
 		info = dbcooper.execute("SELECT text FROM list WHERE userid = '#{userid}'").join
-		title = dbcooper.execute("SELECT title FROM list WHERE userid = '#{userid}'").join
+		title = dbcooper.execute("SELECT title FROM list WHERE userid = '#{userid}'")
 		info_id = dbcooper.execute("SELECT id FROM list WHERE userid = '#{userid}'").join
+		session[:title] = title
 		slim(:logged_in, locals:{ info:info, info_id:info_id, title:title})
 	end
 
@@ -61,9 +62,42 @@ require 'uri'
 		text = params["text"]
 		dbcooper = SQLite3::Database.new("db/forum.sqlite")
 		userid = dbcooper.execute("SELECT id FROM users WHERE username = '#{username}'").join
+		all_titles = dbcooper.execute("SELECT title FROM list")
+		p all_titles[0][0]
+		p title
+		i = 0
+		while i < all_titles.size
+			if title == all_titles[i][i]
+				redirect('/reposti')
+			else
+				i += 1
+			end
+		end
 		dbcooper.execute("INSERT INTO list ('title', 'text', 'userid') VALUES (?,?,?)", [title, text, userid])
 		redirect('/logged_in/')
 	end
+
+
+	get('/reposti') do
+		slim(:reposti)
+	end
+
+
+
+	get('/logged_in/:title') do
+		username = session[:user]
+		title = params[:title]
+		dbcooper = SQLite3::Database.new("db/forum.sqlite")
+		userid = dbcooper.execute("SELECT id FROM users WHERE username = '#{username}'").join
+		poster_id = dbcooper.execute("SELECT userid FROM list WHERE title = '#{title}'").join
+		poster_name = dbcooper.execute("SELECT username FROM users WHERE id='#{poster_id}'").join
+		info = dbcooper.execute("SELECT text FROM list WHERE userid = '#{userid}'").join
+		info_id = dbcooper.execute("SELECT id FROM list WHERE userid = '#{userid}'").join
+		slim(:post, locals:{ info:info, info_id:info_id, userid:userid, poster_name:poster_name})
+	end
+
+
+
 
 	post('/logout') do
 		session[:user] = nil
